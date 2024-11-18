7e04e3a870fb9b17fc46fd35df5597ad50de3c86
mdrayhanhowlader <rhowlader5314@gmail.com>
2024-11-19 03:02:07 +0600

    c++ basics

---
+++ print.cpp
@@ -0,0 +1,9 @@
+ #include<iostream>
+
+ int main() {
+    std::cout << "Hello world";
+
+    int x = 100;
+    std::cout << x;
+    return 0;
+ }
---
+++ ternary_operator.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 6;
+    x%2 == 0 ? cout << "Even" : cout << "Odd";
+    return 0;
+} 
